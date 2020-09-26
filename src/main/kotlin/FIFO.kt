ce6358eb10a8ace7fd522dc254395cb2a11259f5
Kirill Kondratyuk <kirkondratiuk@gmail.com>
2020-09-26 20:51:34 +0300

    created FIFO algorithm

--- src/main/kotlin/FIFO.kt
+++ src/main/kotlin/FIFO.kt
@@ -1,5 +1,17 @@
 import Components.Act
+import java.util.*
 
-fun executeFIFO(act : Act) {
-
+fun executeFIFO(act : Act): Int {
+    // Это очередб состоящая из страниц, которые занимают какой-то кадр
+    var currentListOfPages : Queue<Int> = LinkedList<Int>()
+    var substitutionsQuantity : Int = 0
+    for (nextPage in act.pages) {
+        if (currentListOfPages.contains(nextPage)) {
+            continue
+        }
+        substitutionsQuantity++
+        currentListOfPages.remove()
+        currentListOfPages.add(nextPage)
+    }
+    return substitutionsQuantity
 }
